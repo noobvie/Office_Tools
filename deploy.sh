bcc7ed4b6e191e5efcc93a143b4c127c1c2eaaa5
Noobvie <test@gmail.com>
2026-03-20 21:59:47 -0400

    fix error 3 tolls p3

--- deploy.sh
+++ deploy.sh
@@ -373,13 +373,15 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
     location ^~ /pay-api/ {
-        proxy_pass         http://127.0.0.1:3001/;
-        proxy_http_version 1.1;
-        proxy_set_header   Host              \$host;
-        proxy_set_header   X-Real-IP         \$remote_addr;
-        proxy_set_header   X-Forwarded-For   \$proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto \$scheme;
-        proxy_read_timeout 60s;
+        proxy_pass            http://127.0.0.1:3001/;
+        proxy_http_version    1.1;
+        proxy_set_header      Host              \$host;
+        proxy_set_header      X-Real-IP         \$remote_addr;
+        proxy_set_header      X-Forwarded-For   \$proxy_add_x_forwarded_for;
+        proxy_set_header      X-Forwarded-Proto \$scheme;
+        proxy_read_timeout    600s;
+        proxy_send_timeout    600s;
+        proxy_request_buffering off;
     }
 
     location ~ /\.                           { deny all; return 404; }
